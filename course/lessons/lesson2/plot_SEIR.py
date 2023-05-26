cc092778e3353af7208c63e9396ca5e8d37909a5
soccermatics <djtsumpter@me.com>
2023-05-26 09:24:11 +0200

    Update plot_SEIR.py

--- course/lessons/lesson2/plot_SEIR.py
+++ course/lessons/lesson2/plot_SEIR.py
@@ -12,17 +12,17 @@ are now,
 
   .. math::
 
-     \begin{aligned}
-     \frac{dS}{dt} & = - \beta S I \\
-     \frac{dE}{dt} & = \beta S I - \delta E\\
-     \frac{dI}{dt} & = \delta E -  \gamma I \\
-     \frac{dR}{dt} & =  \gamma I 
-     \end{aligned}
+    \\begin{aligned}
+        \\frac{dS}{dt} & = & - \\beta S I \\
+        \\frac{dE}{dt} & = & \\beta S I - \delta E\\
+        \\frac{dI}{dt} & = & \delta E -  \gamma I \\
+        \\frac{dR}{dt} & = & \gamma I 
+    \\end{aligned}
 
 Simulating the model
 --------------------
 
-  Assume that :math:`\gamma=1/7` and :math:`\beta=1/5`. Write code to draw a
+  Assume that :math:`\gamma=1/7` and :math:`\\beta=1/5`. Write code to draw a
   graph of :math:`I(t)` as a function of time (:math:`t`) for the cases in which
   (on average) a person is exposed for 1, 5 and respectively 9 days before they are infected.
   Assume that :math:`S(0)=999/1000`, :math:`E(0)=0`
